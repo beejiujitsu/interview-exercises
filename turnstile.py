a09c6e56281fcf554adaf41d915d60f73162abd4
Mark Shields <laebshade@gmail.com>
2020-08-07 11:47:19 -0400

    working turnstile

--- turnstile.py
+++ turnstile.py
@@ -13,30 +13,25 @@ A = [12, 12, 12, 12, 12, 1, 12, 5, 5, 5]
 # everytime given entry to the system, print OPEN and change needed
 
 
-def turnstile(given_tokens):  # OPEN[5, 1]
-    # given_tokens = [12,12,12,12,12,1,12,5,5,5]
+def turnstile(given_tokens):
     total = 0
     for token in given_tokens:
         total = total + token
-        if total == Price:
-            print(f"OPEN[]")
-            total = 0
-        elif total > Price:
-            change_needed = total - Price  # 5
-            change = get_change(change_needed)
+        if total >= Price:
+            change = get_change(total - Price)
             print(f"OPEN{change}")
             total = 0
 
 
-# 10 => [5, 5]
 def get_change(change_needed: int):
-    change = []  # [5]
-    seen = {}
-    for token in Tokens:
-        remaining = change_needed - token
-        if remaining in seen:
-            change.extend((seen[remaining], token))
-        seen[token] += 1
+    change = []
+    remaining = change_needed
+    while remaining != 0:
+        for token in sorted(Tokens, reverse=True):
+            if token <= remaining:
+                change.append(token)
+                break
+        remaining = change_needed - sum(change)
     return change
 
 
